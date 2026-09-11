2c5c199c573c815b2936d8dfd9a8f42c1e1fa428
Yomi <yomismtz@gmail.com>
2026-09-11 12:45:58 -0600

    Corregir scopes de peso en Compose

--- app/src/main/java/com/yomismtz/expedientedeldentista/ui/UiCommon.kt
+++ app/src/main/java/com/yomismtz/expedientedeldentista/ui/UiCommon.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
