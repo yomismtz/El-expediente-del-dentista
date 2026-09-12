c5d1e3353a98a702ed3582ec96489bd17ad88846
Yomi <yomismtz@gmail.com>
2026-09-12 15:24:42 -0600

    Actualizar versión Android a 0.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.yomismtz.expedientedeldentista"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.6.0"
+        versionCode = 8
+        versionName = "0.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
