931dc485a1463ebd816a1f891048943e76dfcca6
Yomi <yomismtz@gmail.com>
2026-09-13 09:05:05 -0600

    v0.17: actualizar versión responsive

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.yomismtz.expedientedeldentista"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "0.16-debug"
+        versionCode = 17
+        versionName = "0.17-debug"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
