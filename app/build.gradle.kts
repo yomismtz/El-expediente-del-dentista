9ef68f598c524dddc1cd343746a6e4e219557a07
Yomi <yomismtz@gmail.com>
2026-09-13 21:34:50 -0600

    v0.21: actualiza versión Android

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.yomismtz.expedientedeldentista"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20
-        versionName = "0.20-debug"
+        versionCode = 21
+        versionName = "0.21-debug"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
