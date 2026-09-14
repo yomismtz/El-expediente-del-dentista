856c5711c34d68edad08c30f8739f53364ccbfe3
Yomi <yomismtz@gmail.com>
2026-09-14 10:16:45 -0600

    Sube Preview a v0.28 para validar arranque limpio

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.yomismtz.expedientedeldentista"
         minSdk = 26
         targetSdk = 35
-        versionCode = 22
-        versionName = "0.27-debug"
+        versionCode = 23
+        versionName = "0.28-debug"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
