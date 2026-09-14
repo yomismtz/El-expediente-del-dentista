9e08d8e90ba729b6fdcd1b76d1c25b9f61c5ac7e
Yomi <yomismtz@gmail.com>
2026-09-14 09:30:15 -0600

    build: bump preview version for startup fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.yomismtz.expedientedeldentista"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "0.21-debug"
+        versionCode = 22
+        versionName = "0.27-debug"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
