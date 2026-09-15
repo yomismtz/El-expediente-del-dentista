92b52743367ab119149630675d967c746a986be3
Yomi <yomismtz@gmail.com>
2026-09-15 09:39:13 -0600

    chore: publica Preview 0.33 con nueva jerarquia

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.yomismtz.expedientedeldentista"
         minSdk = 26
         targetSdk = 35
-        versionCode = 27
-        versionName = "0.32-debug"
+        versionCode = 28
+        versionName = "0.33-debug"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
