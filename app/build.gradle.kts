236ba7958b7bd2457000a3a0a593fbca3393b485
Yomi <yomismtz@gmail.com>
2026-09-14 23:37:14 -0600

    chore: publica Preview 0.31 con calculadoras pediátricas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.yomismtz.expedientedeldentista"
         minSdk = 26
         targetSdk = 35
-        versionCode = 25
-        versionName = "0.30-debug"
+        versionCode = 26
+        versionName = "0.31-debug"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
