676664f2818178fa95488a45269287aeed728285
Yomi <yomismtz@gmail.com>
2026-09-14 22:40:40 -0600

    chore: publica Preview 0.30 con corrección de arranque

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.yomismtz.expedientedeldentista"
         minSdk = 26
         targetSdk = 35
-        versionCode = 24
-        versionName = "0.29-debug"
+        versionCode = 25
+        versionName = "0.30-debug"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
