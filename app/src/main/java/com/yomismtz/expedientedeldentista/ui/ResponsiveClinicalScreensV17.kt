8e782b53db8c5ada80669cd6d4016be4aede0787
Yomi <yomismtz@gmail.com>
2026-09-23 16:31:21 -0600

    Corrige importación de navegación protésica

--- app/src/main/java/com/yomismtz/expedientedeldentista/ui/ResponsiveClinicalScreensV17.kt
+++ app/src/main/java/com/yomismtz/expedientedeldentista/ui/ResponsiveClinicalScreensV17.kt
@@ -1,5 +1,6 @@
 package com.yomismtz.expedientedeldentista.ui
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
