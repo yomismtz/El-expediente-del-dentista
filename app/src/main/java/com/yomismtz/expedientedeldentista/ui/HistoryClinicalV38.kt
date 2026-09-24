d257a83ba01cb5fae6f93c09a229142f12b8f0ef
Yomi <yomismtz@gmail.com>
2026-09-24 14:51:09 -0600

    corrige import LazyRow paso 12

--- app/src/main/java/com/yomismtz/expedientedeldentista/ui/HistoryClinicalV38.kt
+++ app/src/main/java/com/yomismtz/expedientedeldentista/ui/HistoryClinicalV38.kt
@@ -2,6 +2,7 @@ package com.yomismtz.expedientedeldentista.ui
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
