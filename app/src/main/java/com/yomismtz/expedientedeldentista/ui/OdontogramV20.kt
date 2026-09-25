8f285a31d7bd5726e58c72c35bcf58800a5bcd55
Yomi <yomismtz@gmail.com>
2026-09-25 16:32:52 -0600

    fix composable odontogram quadrant renderer

--- app/src/main/java/com/yomismtz/expedientedeldentista/ui/OdontogramV20.kt
+++ app/src/main/java/com/yomismtz/expedientedeldentista/ui/OdontogramV20.kt
@@ -124,7 +124,7 @@ fun OdontogramV20Screen(
         }
 
         val wideQuadrants = profile.width == ScreenWidthV17.EXPANDED && !profile.largeSystemText
-        fun quadrantContent(q:OdontoQuadrantV20) {
+        @Composable fun quadrantContent(q:OdontoQuadrantV20) {
             ResponsiveSectionV17(if(lang=="en")q.titleEn else q.titleEs) {
                 AdaptiveGridV17(q.teeth.size,q.teeth.size){i->
                     val tooth=q.teeth[i]
