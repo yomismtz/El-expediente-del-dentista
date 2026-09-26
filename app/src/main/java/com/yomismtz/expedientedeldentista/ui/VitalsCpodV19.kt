d0aaef4395236cef4ba868485dc47d26e44a2cb1
Yomi <yomismtz@gmail.com>
2026-09-25 20:52:59 -0600

    fix Row import in vital signs screen

--- app/src/main/java/com/yomismtz/expedientedeldentista/ui/VitalsCpodV19.kt
+++ app/src/main/java/com/yomismtz/expedientedeldentista/ui/VitalsCpodV19.kt
@@ -2,6 +2,7 @@ package com.yomismtz.expedientedeldentista.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
